2cb3bfe72da86e4edd6edfed1845f23cdd8fd802
pratheep26 <pratheepknk@gmail.com>
2024-04-06 23:48:56 +0530

    Title

--- src/test/java/ecommerce/ProjectEcommerce/Rithu.java
+++ src/test/java/ecommerce/ProjectEcommerce/Rithu.java
@@ -10,6 +10,9 @@ public class Rithu {
     WebDriver driver = new ChromeDriver();
     driver.get("https://www.google.com");
 
+    String title = driver.getTitle();
+    System.out.println(title);
+
 
   }
 
