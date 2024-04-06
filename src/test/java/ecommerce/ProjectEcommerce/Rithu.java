b135f0f22698f7a5471a58c363dffc585b0d6de8
pratheep26 <pratheepknk@gmail.com>
2024-04-06 23:24:28 +0530

    Dev2 first commit

--- src/test/java/ecommerce/ProjectEcommerce/Rithu.java
+++ src/test/java/ecommerce/ProjectEcommerce/Rithu.java
@@ -1,8 +1,13 @@
 package ecommerce.ProjectEcommerce;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Rithu {
 
   public static void main (String[] args) {
+
+    WebDriver driver = new ChromeDriver();
     
   }
 
